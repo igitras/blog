b0aad640e91bdef5f20f5defca2758f20a530692
Mason <mason@igitras.com>
2016-02-01 00:31:09 +0800

    initial import

--- gateway/src/main/java/com/igitras/security/Application.java
+++ gateway/src/main/java/com/igitras/security/Application.java
@@ -1,17 +1,24 @@
 package com.igitras.security;
 
+import com.igitras.security.filters.CsrfHeaderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
 
 /**
  * Created by mason on 1/25/16.
  */
 @SpringBootApplication
-//@EnableRedisHttpSession
+@EnableZuulProxy
 @EnableOAuth2Sso
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -36,28 +43,23 @@ public class Application {
     //                    .roles("USER", "ADMIN", "READER");
     //        }
     //
-    //        @Override
-    //        protected void configure(HttpSecurity http) throws Exception {
-    //            http.httpBasic()
-    //                    .and()
-    //                    .logout()
-    //                    .and()
-    //                    .authorizeRequests()
-    //                    .antMatchers("/index.html", "/login", "/")
-    //                    .permitAll()
-    //                    .anyRequest()
-    //                    .authenticated()
-    //                    .and()
-    //                    .csrf()
-    //                    .csrfTokenRepository(csrfTokenRepository())
-    //                    .and()
-    //                    .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
-    //        }
-    //
-    //        private CsrfTokenRepository csrfTokenRepository() {
-    //            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
-    //            repository.setHeaderName("X-XSRF-TOKEN");
-    //            return repository;
-    //        }
-    //    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.antMatcher("/**").authorizeRequests()
+                .antMatchers("/index.html", "/login", "/")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf()
+                .csrfTokenRepository(csrfTokenRepository())
+                .and()
+                .addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
+    }
+
+    private CsrfTokenRepository csrfTokenRepository() {
+        HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+        repository.setHeaderName("X-XSRF-TOKEN");
+        return repository;
+    }
 }
